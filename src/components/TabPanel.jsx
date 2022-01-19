f6c8e3959d03584093602f01dd5f177a4e479a9a
David Lazaro Fernandez <davidlazaro20@hotmail.com>
2022-01-19 10:45:04 -0600

    Mapa funcional, credenciales actualizadas, FAQ centrado

--- src/components/TabPanel.jsx
+++ src/components/TabPanel.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-
+import {Grid, Typography} from '@mui/material'
 const TabPanel = (props) => {
     const { children, value, index, ...other } = props;
     return (
         <div>
-            {
-                value === index && ( 
-                    {children}
-                )
-            }
+            <Grid 
+            container 
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            >
+                <Grid item 
+                xl={12}
+                lg={12}
+                md={12}
+                sm={12}
+                xs={12}
+                >
+                    <Typography variant="h6">
+                        Ley y Normas
+                    </Typography>
+                    <p></p>
+                </Grid>
+            </Grid>
         </div>
     )
 }
